4bb1abc8a4c52ee8707dd6875cabc8f7845d698f
T1 <mafilippova>
2023-06-09 21:43:02 +0200

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class Driver {
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        this.driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        this.driver.quit();
+    }
+
+    @Test
+    void test() {
+        this.driver.get("https://google.com");
+    }
+}
